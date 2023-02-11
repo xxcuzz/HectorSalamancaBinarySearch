dc20284e75bbc8b9e7dcd3e235d1bcd31ce922fc
Uladzislau Dupanau <u.dupanau@godeltech.com>
2023-02-11 19:20:02 +0100

    Add project files.

---
+++ HC/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace HC;
+
+public class Program
+{
+    public static void Main()
+    {
+        var chars = new List<char>() { 
+            'A', 'B', 'C', 'D', 
+            'E', 'F', 'G', 'H', 
+            'I', 'J', 'K', 'L', 
+            'M', 'N', 'O', 'P', 
+            'Q', 'R', 'S', 'T', 
+            'U', 'V', 'W', 'X', 
+            'Y', 'Z' 
+        };
+
+        var result = string.Empty;
+        
+        for (; ; )
+        {
+            var ch = FindLetter(chars, 0, chars.Count - 1);
+            result += ch;
+            Console.Clear();
+            Console.WriteLine(result);
+        }
+    }
+
+    private static char FindLetter(List<char> chars, int start, int end)
+    {
+        if (end - start == 0)
+        {
+            return chars[start];
+        }
+
+        var searchString = UpdateSearchString(new List<char>(chars), start, end);
+        ShowSearchString(searchString);
+
+        char key = Console.ReadKey().KeyChar;
+        Console.Write("\b");
+
+        if (char.ToUpper(key) == 'D')
+        {
+            Console.WriteLine("*DING!!!*");
+            return FindLetter(chars, searchString.IndexOf(' '), end);
+        }
+        else
+        {
+            return FindLetter(chars, start, searchString.IndexOf(' ') - 1);
+        }
+    }
+
+    private static List<char> UpdateSearchString(List<char> chars, int start, int end)
+    {
+        int mid = (end + start) / 2;
+        chars.Insert(mid + 1, ' ');
+
+        return chars;
+    }
+
+    private static void ShowSearchString(List<char> chars)
+    {
+        foreach (var ch in chars)
+        {
+            Console.Write(ch);
+        }
+
+        Console.WriteLine();
+    }
+}
